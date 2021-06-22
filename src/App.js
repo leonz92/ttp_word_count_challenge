7e7944d1c2f493e797299af51890fcc343c630a7
Leon Zhao <lzhao92@gmail.com>
2021-06-22 15:10:35 -0400

    feature: added text to speech

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
 
-function App() {
+function App () {
+  function readAloud(e) {
+    e.preventDefault();
+    if ('speechSynthesis' in window) {
+      // Speech Synthesis supported
+      let msg = new SpeechSynthesisUtterance('hi');
+      let selection = window.getSelection().toString();
+      if (selection) {
+        msg.text = selection;
+      } else {
+        msg.text = document.getElementById('input-text').value;
+      }
+      msg.volume = 1;
+      console.log(msg);
+      window.speechSynthesis.speak(msg);
+    } else {
+      // Speech Synthesis Not Supported
+      alert("Sorry, your browser doesn't support text to speech!");
+    }
+  }
+
 	return (
 		<div className='App'>
 			<header className="App-header">
@@ -11,10 +31,10 @@ function App() {
 					<label>Input Text Here:</label>
 					<textarea
 						id='input-text'
-						style={{ width: 65 + 'vw', height: 50 + 'vh' }}
+						style={{ width: 65 + 'vw', height: 30 + 'vh' }}
 					></textarea>
 					<button>Analyze</button>
-          <button>Read Aloud</button>
+          <button onClick={readAloud}>Read Aloud</button>
           <button>Dictionary</button>
           <button>Thesaurus</button>
 				</form>
